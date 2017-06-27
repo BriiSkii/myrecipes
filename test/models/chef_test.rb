71c0c24e431f3af76cb5a4a2aa40df7c625db784
BriSkii <bwhit034@outlook.com>
2017-06-27 02:48:14 -0400

    Built recipes and chef models using TDD

---
+++ test/models/chef_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+  def setup
+    @chef = Chef.new(chefname: "Bri Brii", email: "bri@example.com")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "chefname should be less than 30 characters" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "chef email should not be too long" do
+    @chef.email = "a" * 245 + "@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "email should accept correct format" do
+    valid_emails = %w[user@example.com biscuits@gmail.com RemyMa@yahoo.gov john+smith@co.uk.org]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+
+  test "should reject invalid addresses" do
+    invalid_emails = %w[rain@example drop@example!com drop.name@gmail. top@bats+foo.com yayayaya$]
+    invalid_emails.each do |invalids|
+      @chef.email = invalids
+      assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+
+  test "email should be lowercase before hitting the db" do
+    mixed_email = "JuAnaThON@ExaMpLE.CoM"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email 
+  end
+
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @chef = Chef.create!(chefname: "Remy", email: "remy@example.com")
+    @recipe = @chef.recipes.build(name: "Grilled Corn", description: "Shuck, pluck and grill")
+  end
+
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be at least 5 chars" do
+    @recipe.description = "a" * 3
+    assert_not @recipe.valid?
+  end
+
+  test "description should be no more than 500 chars" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+
+  test "recipe without chef should be invalid" do
+  @recipe.chef_id = nil
+  assert_not @recipe.valid?
+end
+end
