3323791d6d506511edb3a1c68b56d376aac983e3
BriSkii <bwhit034@outlook.com>
2017-07-06 21:11:02 -0400

    Configured the login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    chef = Chef.find_by(email: params[:session][:email].downcase)
+    if chef && chef.authenticate(params[:session][:password])
+      session[:chef_id] = chef.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to chef
+    else
+      flash.now[:danger] = "There was something wrong with your login"
+      render 'new'
+    end 
   end
 
   def destroy
   end
-end 
+end
