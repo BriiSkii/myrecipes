9f339abcb3dcc0156fbd9161c718bdf22dde488f
BriSkii <bwhit034@outlook.com>
2017-07-07 22:32:48 -0400

    Created the comment model with validations and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,6 @@ class Recipe < ApplicationRecord
   belongs_to :chef
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708020349) do
+ActiveRecord::Schema.define(version: 20170708021508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20170708020349) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "chef_id"
+    t.integer "recipe_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
