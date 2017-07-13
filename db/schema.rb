bcee0971d7b8554d1ba510144af5a86a127287e5
BriSkii <bwhit034@outlook.com>
2017-07-13 17:28:31 -0400

    Fixed comments model, added timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708021508) do
+ActiveRecord::Schema.define(version: 20170713193849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20170708021508) do
     t.text "description"
     t.integer "chef_id"
     t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ingredients", force: :cascade do |t|
